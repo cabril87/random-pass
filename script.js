5647d9d0fa8905b53f5d23fcb546bcc6548eb378
Carlos Abril <carlos.abril87@outlook.com>
2021-02-17 15:18:03 -0500

    update script

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ var chooseChar = [];
 // // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
 // // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -21,31 +17,25 @@ function writePassword() {
 
 }
 
-// function inputPassword() {
-//   var password = genratepassword;
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 //generate password function gets called in writePassword function, and it should return the final password
 function generatePassword() {
-  //do random generation here and return the final password in the end
+  //randomly generates and return the final password to the end
   var result = "";
-  //ask the user how many characters they want
+  //the prompt ask the user how many characters they want
   var length = prompt("How many characters in your random password? (between 8 and 128)");
-  if(isNaN(length)){
+  if (isNaN(length)) {
     alert("You must input a number!");
     return generatePassword()
   }
-  if(length < 8 || length > 128){
+  if (length < 8 || length > 128) {
     alert("Please choose numbers between 8 - 128!");
     return generatePassword()
   }
-  
+
   //ask the user if they want to include upper, lower, num, special?
   var includeUpper = confirm("Do you want to include uppercase letters?");
   var includeLower = confirm("Do you want to include lowercase letters?");
@@ -57,31 +47,33 @@ function generatePassword() {
     return generatePassword()
   }
   
-    if(includeUpper){
-      chosenChar.push(upper);
-    }
-    if(includeLower){
-      chosenChar.push(lower);
-    }
-  
-    if(includeNumbers) {
-      chosenChar.push(numbers);
-    }
-  
-    if(includeSpecial) {
-      chosenChar.push(special);
-    }
-  
-  // for (var i = 0; i < length; i++) {
-  //   result += chosenChar.charAt(Math.floor(Math.random() * chosenChar.length));
-  // }
-  // return result;
-  
+  if (includeUpper) {
+    chooseChar.push(uppercase);
+  }
+  if (includeLower) {
+    chooseChar.push(lowercase);
   }
 
-  
-      
+  if (includeNumbers) {
+    chooseChar.push(numbers);
+  }
+
+  if (includeSpecial) {
+    chooseChar.push(special);
+  }
+  console.log(chooseChar)
+//loops randoms numbers and special characters
+  for (var i = 0; i < length; i++) {
+    var choosenString = chooseChar[Math.floor(Math.random() * chooseChar.length)];
+    result += choosenString[Math.floor(Math.random() * choosenString.length)]
+  }
+  return result;
+
+}
 
 
-  
-  
+
+
+
+
+
